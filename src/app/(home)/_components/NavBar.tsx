11f9b41dc032b52ea6fa21d595c3d55c594bc332
Sanjay Kumar Sah <sanjay8797421521@gmail.com>
2024-03-31 20:04:04 +0530

    refactor: add seperate Navbar compoent

---
+++ src/app/(home)/_components/NavBar.tsx
@@ -0,0 +1,57 @@
+'use client';
+
+import Image from 'next/image';
+import { usePathname } from 'next/navigation';
+import { useState } from 'react';
+
+import Typography from '../../../components/ui/Typography/Typography';
+import Link from 'next/link';
+import Button from '../../../components/ui/Button/Button';
+
+import logo from '../../../assets/Ansopedia_logo.svg';
+import style from './navbar.module.scss';
+
+const navItems = [
+  { href: '/', label: 'Home' },
+  { href: '/about', label: 'About' },
+  { href: '/contact', label: 'Contact' },
+  { href: '/blog', label: 'Blog' },
+];
+
+const NavBar = () => {
+  const pathname = usePathname();
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+  return (
+    <div className={style['sticky-nav']}>
+      <nav className={style.nav}>
+        <div className={style.logo}>
+          <Image src={logo} alt="ansopedia" className={style.logo} width={60} height={60} priority />
+          <Typography variant="h3">Ansopedia</Typography>
+        </div>
+        <div className={`${style['menu-tablet']} ${isMobileMenuOpen ? style['menu-mobile'] : ''}`}>
+          <div className={style['menu']}>
+            {navItems.map(({ href, label }) => (
+              <Link
+                key={href}
+                href={href}
+                className={`${style['menu__item']} ${pathname === href ? style['menu__item--active'] : ''}`}
+              >
+                {label}
+              </Link>
+            ))}
+          </div>
+          <Button>Sign in</Button>
+        </div>
+        <div className={style['menu-cta']}>
+          <Button onClick={toggleMobileMenu}>X</Button>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
