687cb0d794543b05f190d8b10d125c163af96b7d
Sanjay Kumar Sah <sanjay8797421521@gmail.com>
2024-03-31 14:36:06 +0530

    feat: add Input component

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -2,12 +2,12 @@ import React, { FC } from 'react';
 import style from './button.module.scss';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  variant: 'text' | 'contained' | 'outlined';
+  variant?: 'text' | 'contained' | 'outlined';
   className?: string;
   children?: React.ReactNode;
 }
 
-const Button: FC<ButtonProps> = ({ children, className, variant, ...props }: ButtonProps) => {
+const Button: FC<ButtonProps> = ({ children, className, variant = 'contained', ...props }: ButtonProps) => {
   const styleClass = `${style[variant]} ${className}`;
 
   return (
